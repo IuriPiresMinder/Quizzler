3b67df171f2047c4628fff6ad803bedf578f1710
Iuri Rocha <iuri.rocha@mindera.com>
2023-12-27 10:03:43 +0000

    feature: Created Question struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Iuri Pires on 27/12/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        self.text = q
+        self.answer = a
+    }
+}
